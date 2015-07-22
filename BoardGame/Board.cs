4a7839c5c34a110674d5d88065bf1b676bd29a75
Pippo <killpippo@hotmail.it>
2015-07-22 13:36:22 +0200

    runtime in progress

--- BoardGame/Board.cs
+++ BoardGame/Board.cs
@@ -7,19 +7,69 @@ namespace Check24.Game
 {
     public class Board
     {
+        #region Enum and constants
+
+        /// <summary>
+        /// Definition of the board status
+        /// </summary>
+        public enum eBoardStatus
+        {
+            /// <summary>
+            /// The board has not initialized propery
+            /// </summary>
+            NotInitialized,
+            /// <summary>
+            /// The game is still in progress
+            /// </summary>
+            GameInProgress,
+            /// <summary>
+            /// The game has finished, all the cells have the same color
+            /// </summary>
+            Completed
+        }
+
+        private const int InvalidColor = -1;
+
+        #endregion
+
         #region Members
 
+        /// <summary>
+        /// Definition of the colors for this round
+        /// </summary>
         public SortedSet<int> ColorList { get; private set; }
 
-        private int[,] Cells { get; set; }
+        /// <summary>
+        /// Cell colors
+        /// </summary>
+        private int[][] Cells { get; set; }
 
-        public int Width { get { return Cells.Length; } }
-        public int Height { get { return Cells.GetLength(0); } }
+        /// <summary>
+        /// Board size based on constructor params
+        /// </summary>
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// Number of action done by the player
+        /// </summary>
+        public int Moves { get; private set; }
+
+        /// <summary>
+        /// Current board and game status
+        /// </summary>
+        public eBoardStatus BoardStatus { get; private set; }
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="_iWidth">Must be positive and more than two</param>
+        /// <param name="_iHeight">Must be positive and more than two</param>
+        /// <param name="_aColorList">Must contains more than two RGB color value</param>
         public Board( int _iWidth, int _iHeight, int [] _aColorList )
         {
             if (_iWidth < 1)
@@ -35,11 +85,29 @@ namespace Check24.Game
             ColorList = new SortedSet<int>();
             for ( int i=0; i < _aColorList.Length; i++ )
             {
+                // invalid color
+                if (_aColorList[i] <= InvalidColor)
+                    throw new ArgumentException("Color must be an ARGB positive value");
+
                 if (ColorList.Add(_aColorList[i]) == false)
                     throw new ArgumentException("Color duplication");
             }
-            
-            Cells = new int[_iWidth, _iHeight];
+
+            Width = _iWidth;
+            Height = _iHeight;
+
+            Cells = new int[Width][];
+            for ( int h=0; h<Width; h++ )
+                Cells[h] = new int[Height];
+
+            // initialize to undefined color, and external entity have to setup the board
+            for (int i = 0; i < Width; i++)
+                for (int j = 0; j < Height; j++)
+                    Cells[i][j] = InvalidColor;
+
+            // initialize other members
+            Moves = 0;
+            BoardStatus = eBoardStatus.NotInitialized;
         }
 
         #endregion
@@ -58,29 +126,59 @@ namespace Check24.Game
         }
 
         /// <summary>
-        /// Set/Get board cell color
+        /// Apply the color of the celss before start the game
         /// </summary>
-        /// <param name="i"></param>
-        /// <param name="j"></param>
-        /// <returns></returns>
-        public int this[int i,int j]
+        /// <param name="_aCellColor"></param>
+        public void InitializeColors( int [][] _aCellColor)
         {
-            get
-            {
-                CellCoordCheck(i,j);
+            if (_aCellColor == null)
+                throw new ArgumentNullException("Invalid cell color params");
 
-                return Cells[i, j];
+            if (_aCellColor.Length != Width || _aCellColor[0].Length != Height)
+                throw new ArgumentOutOfRangeException("Cell color matrix wrong size");
+
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    if (!ColorList.Contains( _aCellColor[i][j] ))
+                        throw new ArgumentOutOfRangeException("Invalid color");
+ 
+                    Cells[i][j] = _aCellColor[i][j];
+                }
             }
 
-            set
-            {
-                CellCoordCheck(i, j);
+            BoardStatus = eBoardStatus.GameInProgress;
 
-                if (!ColorList.Contains(value))
-                    throw new ArgumentOutOfRangeException("Invalid color");
+            CheckGameStatus();
+        }
 
-                Cells[i, j] = value;
-            }
+        /// <summary>
+        /// Apply color to the first cell
+        /// </summary>
+        /// <param name="_oColor"></param>
+        public void ApplyColor( int _iColor )
+        {
+            // not initialized
+            if (BoardStatus == eBoardStatus.NotInitialized)
+                throw new NotImplementedException("Board has not been initialized");
+
+            if (!ColorList.Contains(_iColor))
+                throw new ArgumentOutOfRangeException("Invalid color");
+
+            // color didn't change
+            if (Cells[0][0] == _iColor) return;
+
+            Moves++;
+        }
+
+        /// <summary>
+        /// Verifi if the game has finished or still in progress, apply the change in BoardStatusMember
+        /// </summary>
+        private void CheckGameStatus()
+        {
+            // ignore action if not initialized
+            if (BoardStatus == eBoardStatus.NotInitialized) return;
         }
     }
 }
--- TestBoardDefinition/UnitTestBoardRuntime.cs
+++ TestBoardDefinition/UnitTestBoardRuntime.cs
@@ -6,9 +6,91 @@ namespace TestBoardDefinition
     [TestClass]
     public class UnitTestBoardRuntime
     {
-        [TestMethod]
-        public void TestMethod1()
+        private int[] ColorList = new int[] { 1, 2, 3, 4, 5 };
+        private int[][] BoardValues;
+        private int BoardWidth = 10;
+        private int BoardHeight = 10;
+
+        [TestInitialize]
+        public void InitializeBoardColor()
         {
+            BoardValues = new int[BoardWidth][];
+
+            for ( int i=0; i<BoardWidth; i++ )
+            {
+                BoardValues[i] = new int[BoardHeight];
+
+                // apply some colors
+                for (int j = 0; j < BoardHeight; j++)
+                    BoardValues[i][j] = ColorList[(ColorList.Length - 1) % (i * BoardHeight + j + 1)];
+            }
+        }
+
+        [TestMethod]
+        public void BoardIncompleteInitialization()
+        {
+            var oBoard = new Check24.Game.Board(BoardWidth, BoardHeight, ColorList);
+
+            Assert.AreEqual(0, oBoard.Moves);
+            Assert.AreEqual(Check24.Game.Board.eBoardStatus.NotInitialized, oBoard.BoardStatus);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void BoardIncompletePlay()
+        {
+            var oBoard = new Check24.Game.Board(BoardWidth, BoardHeight, ColorList);
+
+            oBoard.ApplyColor(ColorList[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BoardSetColorMatrixNull()
+        {
+            var oBoard = new Check24.Game.Board(BoardWidth, BoardHeight, ColorList);
+
+            oBoard.InitializeColors(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BoardSetColorMatrixUnexpectedWidthSize()
+        {
+            var oBoard = new Check24.Game.Board(BoardWidth, BoardHeight, ColorList);
+
+            int [][] aBoardValues = new int[BoardWidth + 5][];
+
+            for (int i = 0; i < BoardWidth; i++)
+            {
+                aBoardValues[i] = new int[BoardHeight];
+            }
+
+            oBoard.InitializeColors(aBoardValues);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BoardSetColorMatrixUnexpectedHeightSize()
+        {
+            var oBoard = new Check24.Game.Board(BoardWidth, BoardHeight, ColorList);
+
+            int[][] aBoardValues = new int[BoardWidth][];
+
+            for (int i = 0; i < BoardWidth; i++)
+            {
+                aBoardValues[i] = new int[BoardHeight + 5];
+            }
+
+            oBoard.InitializeColors(aBoardValues);
+        }
+
+        [TestMethod]
+        public void BoardIncompleteInvalidPlayColor()
+        {
+            var oBoard = new Check24.Game.Board(BoardWidth, BoardHeight, ColorList);
+
+            oBoard.InitializeColors(BoardValues);
         }
     }
 }
